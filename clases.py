462cf6a8e7a2d08160acb69268216dee0c2161e4
Maicol Castro <mcastrore@unal.edu.co>
2026-02-20 16:24:06 -0500

    primer commit

---
+++ clases.py
@@ -0,0 +1,7 @@
+#clase 1, clases, son plantillas para darles caracteristicas, 
+#temas como comportamiento de maquina, caracteristicas y funciones
+#modelado es una forma de escritura 
+#OBJETO atributos de la clase, realiza comportamientos definidos en ella
+
+# POO= ABSTRACCION DE de caracteristicas objetos, lo general
+#herencia establece la reutilizacion de objetos 
