ae4ae2e37786f030db57d90dbdc8cd201e4590eb
Jonathan_Garzón <_jogarzonl@unal.edu.co_>
2026-04-25 21:15:38 -0500

    Se implementa la clase Trainer, que contiene un nombre y hasta 6 pokemon. Además la posición de estos puede variar.

--- main.py
+++ main.py
@@ -31,3 +31,27 @@ class Pokemon:
             print(f"{self.nombre} evolucionó al nivel {self.nivel}")
         else:
             print("No puede evolucionar a un nivel menor")
+
+class Trainer:
+    def __init__(self, nombre: str, team: str):
+        self.nombre = nombre
+        self.team = team
+        self.pokemon = []
+
+    def add_pokemon(self, pokemon: Pokemon):
+        if len(self.pokemon) < 6 and pokemon not in self.pokemon:
+            self.pokemon.append(pokemon)
+        else:
+            print("No se puede agregar más Pokémon o el Pokémon ya está en el equipo.")
+
+    def get_active_pokemon(self):
+        if self.pokemon:
+            return self.pokemon[0]  
+        else:
+            print("No tienes Pokémon en tu equipo.")
+            return None
+    def switch_pokemon(self, pokemon_index):
+        if 0 <= pokemon_index < len(self.pokemon):
+            self.pokemon[0], self.pokemon[pokemon_index] = self.pokemon[pokemon_index], self.pokemon[0]
+        else:
+            print("Indice de Pokémon no valido.")
