75b38f50a9b6ae79b32f4257f007e846285778d5
fegonzalez7 <fegonzalezro@unal.edu.co>
2026-04-28 07:25:22 -0500

    chore: aplicar formato

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class TypeRelations:
     def __init__(self) -> None:
         self.type_chart: dict[str, dict[str, float]] = {
@@ -8,36 +9,36 @@ class TypeRelations:
                 "Water": 0.5,
                 "Grass": 2.0,
                 "Electric": 1.0,
-                "Ground": 1.0
+                "Ground": 1.0,
             },
             "Water": {
                 "Fire": 2.0,
                 "Water": 0.5,
                 "Grass": 0.5,
                 "Electric": 1.0,
-                "Ground": 2.0
+                "Ground": 2.0,
             },
             "Grass": {
                 "Fire": 0.5,
                 "Water": 2.0,
                 "Grass": 0.5,
                 "Electric": 1.0,
-                "Ground": 2.0
+                "Ground": 2.0,
             },
             "Electric": {
                 "Fire": 1.0,
                 "Water": 2.0,
                 "Grass": 0.5,
                 "Electric": 0.5,
-                "Ground": 0.0 
+                "Ground": 0.0,
             },
             "Ground": {
                 "Fire": 2.0,
                 "Water": 1.0,
                 "Grass": 0.5,
                 "Electric": 2.0,
-                "Ground": 1.0
-            }
+                "Ground": 1.0,
+            },
         }
 
     def get_effectiveness(self, attack_type: str, defender_types: List[str]) -> float:
@@ -54,6 +55,7 @@ class TypeRelations:
 
         return multiplier
 
+
 class Stats:
     def __init__(
         self,
@@ -62,7 +64,7 @@ class Stats:
         defense: float,
         special_attack: float,
         special_defense: float,
-        speed: float
+        speed: float,
     ):
         self.hp = hp
         self.attack = attack
@@ -73,10 +75,13 @@ class Stats:
 
     def __str__(self):
         return (
-            f"HP: {self.hp}, Attack: {self.attack}, Defense: {self.defense}, "
-            f"Sp. Attack: {self.special_attack}, Sp. Defense: {self.special_defense}, Speed: {self.speed}"
+            f"HP: {self.hp}, ",
+            f"Attack: {self.attack}, Defense: {self.defense}, "
+            f"Sp. Attack: {self.special_attack}, Sp. Defense: {self.special_defense}, ",
+            f"Speed: {self.speed}",
         )
-    
+
+
 class Pokemon:
     def __init__(
         self,
@@ -87,9 +92,8 @@ class Pokemon:
         attack: float = 1,
         defense: float = 0.5,
         level: int = 1,
-        special_ability: str = "None"
+        special_ability: str = "None",
     ) -> None:
-
         self.name = name
         self.types = types
         self.life = life
@@ -98,11 +102,13 @@ class Pokemon:
         self.defense = defense
         self.level = level
         self.special_ability = special_ability
-    
-    def get_stats(self) -> str:        
+
+    def get_stats(self) -> str:
         return f"{self.name} Stats: {self.stats}"
 
-    def attack(self, target: "Pokemon", base_power: float, relations: TypeRelations) -> None:
+    def attack(
+        self, target: "Pokemon", base_power: float, relations: TypeRelations
+    ) -> None:
         attack_type = self.types[0]
 
         multiplier = relations.get_effectiveness(attack_type, target.types)
@@ -132,6 +138,8 @@ class Pokemon:
             print(f"{self.name} evolved to level {self.level}")
         else:
             print("Cannot evolve to same or lower level")
+
+
 class Trainer:
     def __init__(self, nombre: str, team: str, pokemon: List[Pokemon]):
         self.nombre = nombre
@@ -146,13 +154,17 @@ class Trainer:
 
     def get_active_pokemon(self):
         if self.pokemon:
-            return self.pokemon[0]  
+            return self.pokemon[0]
         else:
             print("No tienes Pokémon en tu equipo.")
             return None
+
     def switch_pokemon(self, pokemon_index):
         if 0 <= pokemon_index < len(self.pokemon):
-            self.pokemon[0], self.pokemon[pokemon_index] = self.pokemon[pokemon_index], self.pokemon[0]
+            self.pokemon[0], self.pokemon[pokemon_index] = (
+                self.pokemon[pokemon_index],
+                self.pokemon[0],
+            )
         else:
             print("Indice de Pokémon no valido.")
 
@@ -160,9 +172,15 @@ class Trainer:
 def main() -> None:
     relations = TypeRelations()
 
-    charmander_stats = Stats(hp=20, attack=2, defense=0.5, special_attack=1, special_defense=1, speed=1)
-    bulbasaur_stats = Stats(hp=20, attack=1, defense=0.3, special_attack=1, special_defense=1, speed=1)
-    squirtle_stats = Stats(hp=20, attack=1, defense=0.5, special_attack=1, special_defense=1, speed=1)
+    charmander_stats = Stats(
+        hp=20, attack=2, defense=0.5, special_attack=1, special_defense=1, speed=1
+    )
+    bulbasaur_stats = Stats(
+        hp=20, attack=1, defense=0.3, special_attack=1, special_defense=1, speed=1
+    )
+    squirtle_stats = Stats(
+        hp=20, attack=1, defense=0.5, special_attack=1, special_defense=1, speed=1
+    )
 
     charmander = Pokemon("Charmander", ["Fire"], charmander_stats, life=20, attack=2)
     bulbasaur = Pokemon("Bulbasaur", ["Grass"], bulbasaur_stats, life=20, defense=0.3)
@@ -176,6 +194,7 @@ def main() -> None:
 
     print("\n--- BATTLE 3 ---")
     squirtle.attack(charmander, base_power=5, relations=relations)
+
 
 if __name__ == "__main__":
     main()
